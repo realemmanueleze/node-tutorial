164a7071f35599a4d6979265d31d9a8c6df20dcf
Emmanuel Eze <45445963+realemmanueleze@users.noreply.github.com>
2022-12-24 02:42:41 +0100

    feat: added authorization middleware

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,43 +1,24 @@
-const express = require("express");
-const app = express();
-const { products } = require("./data");
-
-app.get("/", (req, res) => {
-  res.send(`<h1>Home Page</h1><a href='/api/products'>products</a>`);
-});
-
-app.get("/api/products", (req, res) => {
-  const prod = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
-  res.json(prod);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const { id } = req.params;
-  const sp = products.find((product) => product.id === Number(id));
-  res.json(sp);
-});
-
-app.get("/api/v1/query", (req, res) => {
-  console.log(req.query);
-  const { search, limit } = req.query;
-  let sorted = [...products];
-  if (search) {
-    sorted = sorted.filter((product) => {
-      return product.name.startsWith(search);
-    });
-  }
-  if(limit){
-    sorted = sorted.slice(0,Number(limit))
-  }
-  if(sorted.lenght < 1){
-    res.status(200).send('no products matched your search')
-  }
-  res.json(sorted);
-});
+const express = require('express')
+const app = express()
+const logger = require('./logger')
+const authorize = require('./authorize')
+//  req => middleware => res
+app.use([logger, authorize])
+// api/home/about/products
+app.get('/', (req, res) => {
+  res.send('Home')
+})
+app.get('/about', (req, res) => {
+  res.send('About')
+})
+app.get('/api/products', (req, res) => {
+  res.send('Products')
+})
+app.get('/api/items', (req, res) => {
+  console.log(req.user)
+  res.send('Items')
+})
 
 app.listen(5000, () => {
-  console.log("listening on port 5000....");
-});
+  console.log('Server is listening on port 5000....')
+})
